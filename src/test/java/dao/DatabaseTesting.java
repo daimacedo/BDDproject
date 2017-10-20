e004c1e9cb0062cd6287d9f22cb14893b9f906c3
Daiane <daiane.macedo@hotmail.com>
2017-10-20 18:02:43 -0200

    Gerando Select e obtendo dados de conexao atraves de arquivo local

--- src/test/java/dao/DatabaseTesting.java
+++ src/test/java/dao/DatabaseTesting.java
@@ -1,24 +1,50 @@
 package dao;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Properties;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+
+import gherkin.formatter.model.Result;
 
 
 public class DatabaseTesting {
 
-	public static void main(String[] args) throws ClassNotFoundException {
 
-		String usuario = "nconfidence";
-		String senha = "nconfidence_tst";
-		String url = "jdbc:jtds:sqlserver://10.90.0.9:62501;databaseName=nconfidence;instance=tst";
+	public static Connection conectarBancoTeste() throws IOException
+	{
 		
+		
+		
+		String pathname = "/home/daiane.macedo/workspace2/test.properties";
+		Properties prop = new Properties();
+		FileInputStream fs = new FileInputStream(pathname);
+		prop.load(fs);
+		
+		String usuario = prop.getProperty("usuario");
+		String senha = prop.getProperty("senha");
+		String url = prop.getProperty("url");
+		System.out.println(url);
+		Connection conexao = null;
 
 
 		try {
 			 Class.forName("net.sourceforge.jtds.jdbc.Driver");
-			 Connection conexao = DriverManager.getConnection(url, usuario, senha);
 			 System.out.println("Conexão obtida com sucesso");
+			 conexao = DriverManager.getConnection(url, usuario, senha);
+			 System.out.println(conexao);
+			
 			 
 		} catch (ClassNotFoundException e1) {
 
@@ -27,5 +53,48 @@ public class DatabaseTesting {
 
 			e1.printStackTrace();
 		}
+		return conexao;
+
 	}
+	
+	
+	@Test
+	public void testaConsultaBancoDados() throws SQLException, IOException
+	{
+		Connection conn = conectarBancoTeste();
+		
+		String sql = "SELECT numeroSemFormato, pessoa_id FROM Documento where numeroSemFormato = '41844043835'";
+		PreparedStatement ps = conn.prepareStatement(sql);
+		ResultSet rs = ps.executeQuery();
+		while(rs.next())
+		{
+			String documento  = rs.getString("numeroSemFormato");
+			String idPessoa = rs.getString("pessoa_id");
+			System.out.println("Numero do documento: " + documento );
+			System.out.println("Id pessoa: " + idPessoa);
+			assertThat(documento).isEqualTo("41844043835");
+			assertThat(idPessoa).isEqualTo("5114346");
+			
+		}
+		
+		conn.close();
+		
+		if(conn.isClosed())
+			{
+			 System.out.println("fechou");
+			}
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	public static void main(String[] args) throws ClassNotFoundException, IOException {
+
+		conectarBancoTeste();
+	}
+		
 }
